6ae3d85533daea0157af358fb9ec68d253e72517
Johan Castillo <jcjohan2707@gmail.com>
2020-11-30 15:25:02 -0400

    Hello world dont use JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,6 @@
-import logo from './logo.svg';
+import React from 'react';
 import './styles/styles.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => <h1>Hola mundo</h1>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const root = document.getElementById('root');
+const element = React.createElement("h1", {className: "saludo"}, "Hola mundo");
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  element,
+  root
 );
 
 // If you want to start measuring performance in your app, pass a function
