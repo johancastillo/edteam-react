aac96c532c9f8750a314f2cbbe05b66acdc3480c
Johan Castillo <jcjohan2707@gmail.com>
2020-12-01 14:57:17 -0400

    My first component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => (
         <div class="main-banner img-container l-section" id="main-banner">
             <div class="ed-grid lg-grid-6">
                 <div class="lg-cols-4 lg-x-2">
-                    <img class="main-banner__img" src="tu-imagen.jpg" />
+                    <img class="main-banner__img" src="https://manchainformacion.es/system/previews/91894/original/por-que-el-mejor-estudiante-de-la-clase-nunca-se-hace-rico.jpg" />
                     <div class="main-banner__data s-center">
                         <p class="t2 s-mb-0">Título del banner</p>
                         <p> Subtítulo del banner</p>
