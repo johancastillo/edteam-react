2057816408252372b955b6d467c4f15967846d15
Johan Castillo <jcjohan2707@gmail.com>
2020-12-01 10:19:00 -0400

    Sass code use

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import './styles/styles.scss';
 
-const App = () => <h1>Hola mundo</h1>;
+const App = () => (
+    <>
+        <h1 className="text-center">Hola mundo</h1>
+        <p className="text-center">Desde el componente App</p>
+    </>
+);
 
 export default App;
