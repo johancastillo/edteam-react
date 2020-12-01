a58ac290f7f6ff6a9f1e010b6370e5899ed6afc9
Johan Castillo <jcjohan2707@gmail.com>
2020-11-30 21:36:48 -0400

    Restrictions for JSX

--- src/App.js
+++ src/App.js
@@ -4,3 +4,12 @@ import './styles/styles.scss';
 const App = () => <h1>Hola mundo</h1>;
 
 export default App;
+
+// Reglas JSX
+// 1. Toda etiqueta debe cerrarse.
+// 2. Los componentes deben retornar un solo elemento padre.
+// 3. Apoyarse de los Fragments cuando necesitemos retornar 2 elementos.
+// 4. <Fragment> </Fragment> => <> </>
+// 5. <img> siempre se cierran.
+// 6. class de CSS pasa a ser className
+// 7. for => htmlFor
