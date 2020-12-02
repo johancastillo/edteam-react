89a9b493b20dcf683361f7a99b8d8230d8814904
Johan Castillo <jcjohan2707@gmail.com>
2020-12-02 15:00:46 -0400

    Destructuring object props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,27 +1,27 @@
 import React from 'react';
 
-const Card = props => (
+const Card = ({title, image, imageTeacher, teacher, price}) => (
     <article class="card">
         <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src={props.image} alt={props.title} />
+            <img src={image} alt={title} />
         </div>
         <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 class="t5 s-mb-2 s-center">
-                {props.title}
+                {title}
             </h3>
             <div class="s-mb-2 s-main-center">
                 <div class="card__teacher s-cross-center">
                     <div class="card__avatar s-mr-1">
                         <div class="circle img-container">
-                            <img src={props.imageTeacher} alt="profesor" />
+                            <img src={imageTeacher} alt="profesor" />
                         </div>
                     </div>
-                    <span class="small">{props.teacher}</span>
+                    <span class="small">{teacher}</span>
                 </div>
             </div>
             <div class="s-main-center">
                 <a class="button--ghost-alert button--tiny" href="#">
-                    {`$ ${props.price}`}
+                    {`$ ${price}`}
                 </a>
             </div>
         </div>
