20d694d3d981e1adc064f64ab147ecd98283b442
Johan Castillo <jcjohan2707@gmail.com>
2020-12-02 14:50:29 -0400

    Component dinamic with Props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,35 +1,28 @@
 import React from 'react';
 
-const persona = {
-    "nombre": "Johan",
-    "apellido": "Castillo",
-    "edad": 17
-}
-
-const mayorDeEdad = edad => edad >= 18
-
-const Card = () => (
+const Card = props => (
     <article class="card">
         <div class="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-            <img src="https://ux.ed.team/img/curso-portada.png" alt="poster del curso" />
+            <img src={props.image} alt={props.title} />
         </div>
         <div class="card__data s-border s-radius-br s-radius-bl s-pxy-2">
             <h3 class="t5 s-mb-2 s-center">
-                Programación orientada a objetos con Go
+                {props.title}
             </h3>
             <div class="s-mb-2 s-main-center">
                 <div class="card__teacher s-cross-center">
                     <div class="card__avatar s-mr-1">
                         <div class="circle img-container">
-                            <img src="https://ux.ed.team/img/profesor.jpg" alt="profesor" />
+                            <img src={props.imageTeacher} alt="profesor" />
                         </div>
                     </div>
-                    <span class="small">{`${persona.nombre} ${persona.apellido}`}</span>
-                    <p>{ mayorDeEdad(persona.edad) ? "Soy mayor" : "Soy menor"}</p>
+                    <span class="small">{props.teacher}</span>
                 </div>
             </div>
             <div class="s-main-center">
-                <a class="button--ghost-alert button--tiny" href="#">$ 20USD</a>
+                <a class="button--ghost-alert button--tiny" href="#">
+                    {`$ ${props.price}`}
+                </a>
             </div>
         </div>
     </article>
