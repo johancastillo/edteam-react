b6f920905bc3d9213ae6d96084299820bf1fe039
Johan Castillo <jcjohan2707@gmail.com>
2020-12-04 15:22:29 -0400

    From in JSX

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -10,6 +10,23 @@ class Formulario extends Component{
         return(
             <div className="ed-grid">
                 <h1>Formulario</h1>
+
+                <form>
+                    <div className="form__item">
+                        <label>Nombre completo</label>
+                        <input type="text"/>
+                    </div>
+
+                    <div className="form__item">
+                        <label>Correo electrónico</label>
+                        <input type="email"/>
+                    </div>
+
+                    <div className="form__item">
+                        <input className="button full" type="submit" value="Enviar"/>
+                    </div>
+                </form>
+
             </div>
         )
     }
